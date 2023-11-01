03be95cb8c3b30572ff2713bc2d2f5dbc2ca7a81
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-11-02 02:21:21 +0600

    basic datatypes decleration

---
+++ module1/src/basicDataType.ts
@@ -0,0 +1,25 @@
+// string
+let firstName : string = 'Al Tahmiduzzaman Tuhin';
+// number
+let roll : number = 114;
+// boolean
+let isStudent : boolean = false;
+// undefine
+let x : undefined = undefined;
+// null
+let y : null = null;
+
+// declare a variable without value
+
+let lastName : string;
+// ----------------------------------
+
+// Array
+
+let friends : string[] = ['maruf', 'sawon','shuvo'];
+
+let rollList :number[] = [105,118,103];
+
+// tuple --> An Array maintain order - types of values
+
+let nameAge : [string , number] = ['tuhin', 114];
