fb78d24bacac000d4bb8d6daec3b2456ed7cad6f
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-11-02 11:14:01 +0600

    Refference Type --> Object type | optional type | literal type

--- module1/src/basicDataType.ts
+++ module1/src/basicDataType.ts
@@ -23,3 +23,21 @@ let rollList :number[] = [105,118,103];
 // tuple --> An Array maintain order - types of values
 
 let nameAge : [string , number] = ['tuhin', 114];
+
+// -------------module 1.5------------------
+
+// reference type
+    // object
+    const user: {
+        // readonly company : string; // fixed a property value using access modifier
+        company : 'Programming Hero'; //type --> literal type
+        firstName : string;
+        middleName? : string | undefined; //optinal type
+        lastName : string;
+        isStudent : boolean;
+    } = {
+        company : 'Programming Hero',
+        firstName : 'Habib',
+        lastName : 'Shawon',
+        isStudent : true
+    }
