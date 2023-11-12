fb3e0565f74f4f08d06e12c1bb44b76a1b39d419
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-11-13 02:35:22 +0600

    Learning & practice Intersection & Union in TypeScript

--- module1/src/1.10.ts
+++ module1/src/1.10.ts
@@ -1,8 +1,64 @@
 {
     // Union & Intersection
+    
+    // ------- Union -> Like as Logical OR(||) operation ---------
 
-    type FrontENdDeveloper = 'fakibaziDeveloper' | 'junior Developer'
+    // N:B: The string literal type allows you to specify a set of possible string values for a variable, only those string values can be assigned to a variable.
+    //string literal types 👇(FrontENdDeveloper)
+    type FrontENdDeveloper = 'Copy Paste Developer' | 'Junior Developer';
+
+    type FullStactDeveloper = 'Frontend Developer' | 'Backend Developer';
+
+    type Developer = FrontENdDeveloper | FullStactDeveloper;
+
+    
+    const newDeveloper : FrontENdDeveloper = 'Junior Developer';
+
+
+    type User = {
+        name : string;
+        age : number;
+        address : string;
+        contactNo : string;
+        gender : "male" | "Female"; 
+        bloodGroup : "O+"|"A+"|"B+"|"AB+";
+        email? : string;
+    }
+
+    const user1: User = {
+        name : 'tuhin',
+        age : 24,
+        address : "Ghana",
+        contactNo : "01798908900",
+        gender : "male",
+        bloodGroup : "B+",
+        email : "tuhin@gmail.com"
+
+
+    }
+
+    // ------- Intersection -> Like as Logical AND (&&) operation ---------
+
+    type FrontEndDeveloper = {
+        skills : string[];
+        designation1 : "Frontend Developer"
+    }
+    type BackEndDeveloper = {
+        skills : string[];
+        designation2 : "Backend Developer"
+    }
+
+    type FullStackDeveloper = FrontEndDeveloper & BackEndDeveloper;
+
+    const fullStackDeveloper : FullStackDeveloper ={
+        skills : ["HTML", "CSS" , "JS" , "nodeJS" , "ExpressJS" , "MongoDB" ,"React" ],
+        designation1 : "Frontend Developer",
+        designation2 : "Backend Developer"
+    }
+
  
+
+
 
 
 
