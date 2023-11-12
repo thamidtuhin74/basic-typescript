b4f5778784963cbff9d8bbc3c1fd5aeef97cc7fe
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-11-13 00:07:50 +0600

    learn spread operator

--- module1/src/1.7.ts
+++ module1/src/1.7.ts
@@ -6,8 +6,32 @@
 
 
     //Spread operator */
+
+    const bro1 : string[] = ['x','y','z'];
+    const bro2 : string[] = ['a','b','c'];
+
+    bro1.push(...bro2); //spread operator for array(...)
+    console.log(bro1);
+
+    //spread operator for Object (...)
+    const mentors1 = {
+        typeScript : 'mezba',
+        redux : 'mir',
+        dbms : 'mizan'
+    }
+    const mentors2 = {
+        prisma : 'firoz',
+        nextjs : 'tonmoy',
+        cloud : 'nahid'
+    }
+
+    const mentorList = {
+        ...mentors1,
+        ...mentors2
+    }
+
+    // -------------------------
+
+    // Rest Operator
     
-    const poorUser = {
-    name: 'Tuhin'
-}
 }
