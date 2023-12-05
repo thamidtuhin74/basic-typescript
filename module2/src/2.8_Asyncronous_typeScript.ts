c69c85ac7e4077f83dbb87914af05665159a099f
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-12-06 01:08:35 +0600

    Promise in typescript & fetch data using fetch function

--- module2/src/2.8_Asyncronous_typeScript.ts
+++ module2/src/2.8_Asyncronous_typeScript.ts
@@ -3,9 +3,10 @@
     // promise funtion
 
 
-    // simulation
-    const createPromise = () =>{
-        return new Promise((resolve , reject)=>{// promise funtion
+    // Custome Data load
+    // simulation for a String / boolean etc
+    const createPromise = (): Promise<string> =>{
+        return new Promise<string>((resolve , reject)=>{// promise funtion
             const data : string = "Something";
         
             if(data){
@@ -18,11 +19,55 @@
 
     }
     // calling promise function
-    const showData = async () =>{
-        const data = await createPromise();
+    const showData = async () : Promise<string>=>{
+        const data: string = await createPromise();
         console.log(data);
+        return data
     }
     showData();
+    
+    // simulation for a Object
+
+    type Something = {something:string}
+    const createPromiseForObject = (): Promise<Something> =>{
+        return new Promise<Something>((resolve , reject)=>{// promise funtion
+            const data : Something = {something:"Something from Object"};
+        
+            if(data){
+                resolve(data); 
+            }
+            else{
+                reject('failed to load data');
+            }
+        })
+
+    }
+    // calling promise function
+    const showDataOfObject = async () : Promise<Something>=>{
+        const data: Something = await createPromiseForObject();
+        console.log(data.something);
+        return data
+    }
+    showData();
+
+
+
+    // Data Load from server side
+    type ToDo = {
+        userId : number;
+        id : number;
+        title : string;
+        completed: false;
+    }
+
+    const getToDo = async () : Promise<ToDo> => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
+
+    getToDo();
 
 
 }
