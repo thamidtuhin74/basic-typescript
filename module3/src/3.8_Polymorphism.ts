8a2567c358f506dae4187606f9380510f9642073
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-12-06 15:12:43 +0600

    Ploymorphism with shape

--- module3/src/3.8_Polymorphism.ts
+++ module3/src/3.8_Polymorphism.ts
@@ -31,4 +31,50 @@
     getSleepingHour(dev1);
 
 
+
+    //------Another Example----------
+
+    class Shape{
+        constructor(public name: string){
+
+        }
+        getArea(): number{
+            return 0;
+        }
+    }
+
+    class Circle extends Shape{
+        constructor(public radius: number) {
+           super("Circle"); 
+        }
+        getArea(): number {
+            return Math.PI * this.radius * this.radius;
+        }
+
+    }
+
+    class Rectangle extends Shape{
+        constructor(public height: number , public width: number) {
+           super("Rectangle"); 
+        }
+        getArea(): number {
+            return this.height * this.width;
+        }
+
+    }
+
+    const getShapeArea = (param : Shape) =>{
+        console.log(`Area of a ${param.name} is = ${param.getArea()}`);
+    }
+
+    const Shape1 = new Shape("Any shape");
+    const Circle1 = new Circle(5);
+    const Rect1 = new Rectangle(10,6);
+
+    getShapeArea(Shape1);
+    getShapeArea(Circle1);
+    getShapeArea(Rect1);
+
+
+
 }
